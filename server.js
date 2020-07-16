a42718639a8f57390b15e1715977ba8a5b5195b2
Align-center <lucasfrech@hotmail.fr>
2020-07-16 16:04:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+var port = process.env.PORT || 3000;
+
 var options = {
     dotfiles: 'ignore',
     etag: false,
